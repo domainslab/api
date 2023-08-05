60fd3f602ad3dd1d64ae5654540fc3919c3fac1d
azrv <yellowbang368@gmail.com>
2023-08-05 17:13:23 +0300

    CORS Add allowed origins
    add types for cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { router as domainRouter } from './controllers';
 import { errorHandler } from './middlewares/errorHandler';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(domainRouter);
 app.use(errorHandler);
