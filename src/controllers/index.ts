11e6640dc60aa077a21d45dbf4a1b816de2cc3d9
Bogdan Bogdanov <greenwookez@gmail.com>
2023-08-18 11:38:47 +0300

    refactor: remove /api prefix from routes since we use api subdomain

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { domainRouter } from './domains.controller';
 
-const V1 = '/api/v1';
+const V1 = '/v1';
 
 const router = Router();
 router.use(V1, domainRouter);
