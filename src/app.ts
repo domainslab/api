2f340a55ad9ae154d3ed1c19073b772b7c6bf742
Bogdan Bogdanov <greenwookez@gmail.com>
2023-10-22 12:42:31 +0300

    fix: app use router missing

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import errorHandler from './middlewares/errorHandler';
+import { router } from './controllers';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(router);
 app.use(errorHandler);
 
 export default app;
