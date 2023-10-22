9388a5c474311661f10478f159e83485b99929ca
Bogdan Bogdanov <greenwookez@gmail.com>
2023-10-22 12:31:35 +0300

    fix: disable auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import express from 'express';
-import { router } from './controllers';
 import { errorHandler } from './middlewares/errorHandler';
 import cors from 'cors';
-import { authenticateRequestMiddleware } from './middlewares/authenticateRequest';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(authenticateRequestMiddleware, router);
 app.use(errorHandler);
 
 export default app;
