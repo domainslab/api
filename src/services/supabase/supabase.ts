0e9908a87b828afea23172813e127bb648696673
artemkonovalov <Tema2712>
2023-11-14 16:35:39 +0300

    #5 - type for supabase client instance

--- src/services/supabase/supabase.ts
+++ src/services/supabase/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '../../../database.types';
 
 const URL = process.env.SUPABASE_URL as string;
 const KEY = process.env.SUPABASE_KEY as string;
 
-const supabase = createClient(URL, KEY);
+const supabase = createClient<Database>(URL, KEY);
 
 export default supabase;
