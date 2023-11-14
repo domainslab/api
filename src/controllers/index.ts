4607f168ab95a15176b38e375d581a181203d096
artemkonovalov <Tema2712>
2023-11-14 16:32:00 +0300

    #5 - index.ts for controller directory

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { domainRouter } from './domains.controller';
+import { domainRouter } from './domains';
+import { postsRouter } from './posts';
 
 const V1 = '/v1';
 
 const router = Router();
 router.use(V1, domainRouter);
-
+router.use(V1, postsRouter);
 export { router };
