122cdcc3715d9db5afff40a2025f1f81233694c4
artemkonovalov <Tema2712>
2023-11-14 16:32:15 +0300

    #5 - index.ts for services directory

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
 export { getDomains, isDescValid } from './ai';
 export { isDomainAvailable } from './goDaddy';
+export {getPostsByPage, getPostByID, deletePostById} from './supabase';
--- src/services/supabase/index.ts
+++ src/services/supabase/index.ts
@@ -1,1 +1,1 @@
-export { default } from './supabase';
+export * from './dbFunctions';
